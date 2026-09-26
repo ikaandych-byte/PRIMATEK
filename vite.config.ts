af198f85bc6aeda68b5723644bec27c174775a71
ikaandych-byte <ikaandych@gmail.com>
2026-09-26 17:53:34 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/', // <--- Penyesuaian ditambahkan di sini agar aset terbaca di root domain pttid.com
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
